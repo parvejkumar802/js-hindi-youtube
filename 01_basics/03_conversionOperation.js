c3e364fd65036db45d81bc79e048a0875904fcb9
parvejkumar802 <83976164+parvejkumar802@users.noreply.github.com>
2024-02-06 08:23:20 +0000

    conversionPractics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "naam"
+// //console.log(typeof(score));
+
+// //let nowscore = Number(score);
+// //console.log(typeof nowscore);
+// //console.log(nowscore);
+
+// let score = false;
+// console.log(typeof(score));
+
+// let nowscore = Number(score);
+// console.log(typeof nowscore);
+// console.log(nowscore);
+
+// let score = "33nghhh";
+// console.log(typeof(score));
+
+// let nowscore = Number(score);
+// console.log(typeof nowscore);
+// console.log(nowscore);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+console.log(typeof stringNumber);
