62c77b29f4fa73a475189e442dbbc89985b50c46
parvejkumar802 <83976164+parvejkumar802@users.noreply.github.com>
2024-02-06 08:49:38 +0000

    compariosion experiment with diff datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//please avoid above comparision because it could be confusing with output 
+
+// === (its a strict check)
+
+console.log("2" === 2);
